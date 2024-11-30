d78fa124d8fd085b87e599786dd5f123606a998d
ShirShamay <shirshamay19454@outlook.com>
2024-11-30 18:59:39 +0200

    updating files + adding smooth scrolling with JS

--- Shiri website/script.js
+++ Shiri website/script.js
@@ -1,17 +1,35 @@
+let btnAbout = document.querySelector('.btn-about')
+let btnContact = document.querySelector('.contact')
 
+function jump_to() {
+  let elementToJump = document.getElementById('about-info');
+  console.log(elementToJump)
+  if (elementToJump) {
+    let top = elementToJump.getBoundingClientRect().top;
+    window.scrollTo({
+      top: top,
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
+    console.error('Element not found');
+  }
+}
 
-
-
-
- function jump_to(el_id) {
-   console.log("yeahhhh");
-   var el_to_jump = document.getElementById(el_id);
-   console.log(el_to_jump);
-   var top = el_to_jump.getBoundingClientRect().top;
-   console.log(top);
-   window.scrollTo({
-     top: top,
-     left: 0,
-     behavior: "smooth",
-   });
- }
+btnAbout.addEventListener('click', jump_to)
+ 
+function jump_end() {
+  let elementJump = document.getElementById('end-page')
+  console.log(elementJump)
+  if (elementJump) {
+    let top = elementJump.getBoundingClientRect().top;
+    window.scrollTo({
+      top: top,
+      left: 0,
+      behavior:"smooth"
+    })
+  } else {
+    console.error('element not found')
+  }
+}
+btnContact.addEventListener('click', jump_end)
